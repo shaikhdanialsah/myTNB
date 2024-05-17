f172f7abba0a21b38292456a0fa19d7ed9902593
Shaikh <shaikhdanial02@gmail.com>
2024-05-18 01:25:09 +0800

    Commit

--- app/src/main/java/dev/lab/electriccalculator/MainActivity.java
+++ app/src/main/java/dev/lab/electriccalculator/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity{
                     Intent intent = new Intent(Intent.ACTION_SEND);
                     intent.setType("text/plain");
                     intent.putExtra(Intent.EXTRA_SUBJECT, "Check out my awesome app!");
-                    intent.putExtra(Intent.EXTRA_TEXT, "Hey there! I've developed this cool app. Check it out!");
+                    intent.putExtra(Intent.EXTRA_TEXT, "Hey there! I've developed this cool app. Check it out on GitHub!");
                     intent.putExtra(Intent.EXTRA_TEXT, "https://github.com/shaikhdanialsah/ICT602");
                     startActivity(Intent.createChooser(intent, "Share via"));
                     return true;
